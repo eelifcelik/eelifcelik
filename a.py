c2e456f89fb23573ae6f803a10583001b112f0d3
onurravli <onurravli@hotmail.com>
2022-08-16 22:09:20 +0300

    I've formatted python code, corrected some typos, etc. Also, using png is not a good idea. Instead, I recommend to use webp.

---
+++ a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+class ComputerEngineeringStudent:
+    def __init__(self):
+        self.name = "Elif Çelik"
+        self.role = "Computer Engineer Student"
+        self.language_spoken = ["Turkish_TR", "English_EN"]
+
+    def say_hi(self):
+        print("Welcome to my GitHub page!")
+
+
+me = ComputerEngineeringStudent()
+me.say_hi()
